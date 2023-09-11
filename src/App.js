4dd5e4701909409cc314f81894d72bc70e3f41e7
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-09-11 16:09:11 +0530

    Setting up an react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
